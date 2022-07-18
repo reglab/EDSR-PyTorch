9c6d0939c05cfa2ae9acc5f53f8c0377d5d739a0
Nathan Jo <nathanjo@sls-314-mbpro2021.lan>
2022-07-18 17:03:59 -0400

    new bash file

---
+++ src/demo2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --begin=now
+#SBATCH --job-name=rgb-footprint-extract
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nathanjo@law.stanford.edu
+#SBATCH --partition=owners
+#SBATCH --time=00:30:00
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+
+singularity exec --nv $GROUP_HOME/singularity/rgb-building1.sif python3 main.py --data_test Demo --scale 4 --pre_train download --test_only --save_results
